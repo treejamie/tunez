41d422648610550b44a6b5100fe322153d8f715a
Tree Jamie <me@treejamie.com>
2025-06-11 20:26:22 +0100

    now with validations

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -43,4 +43,22 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  validations do
+    validate numericality(
+        :year_released,
+        greater_than_or_equal_to: 1950,
+        less_than_or_equal_to: &__MODULE__.next_year/0
+      ),
+      where: [present(:year_released)],
+      message: "must be between 1950 and next year"
+
+      validate match(:cover_image_url,
+        ~r"(^https://|/images/).+$"
+      ),
+      where: [changing(:cover_image_url)],
+      message: "must start with https:// or /images"
+  end
+
+  def next_year, do: Date.utc_today().year + 1
+
 end
