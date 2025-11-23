c88d5758c937f8bf5eaedcdf4664a060980c95b2
treejamie <me@treejamie.com>
2025-11-23 09:34:01 +0000

    done with calculations and moved them into aggregates

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -19,7 +19,8 @@ defmodule TunezWeb.Artists.IndexLive do
     page =
       Tunez.Music.search_artists!(query_text,
         page: page_params,
-        query: [sort_input: sort_by]
+        query: [sort_input: sort_by],
+        load: [:album_count, :latest_album_year_released, :cover_image_url]
       )
 
     socket =
@@ -67,7 +68,7 @@ defmodule TunezWeb.Artists.IndexLive do
     ~H"""
     <div id={"artist-#{@artist.id}"} data-role="artist-card" class="relative mb-2">
       <.link navigate={~p"/artists/#{@artist.id}"}>
-        <.cover_image />
+        <.cover_image image={@artist.cover_image_url} />
       </.link>
     </div>
     <p class="flex justify-between">
@@ -79,6 +80,8 @@ defmodule TunezWeb.Artists.IndexLive do
         {@artist.name}
       </.link>
     </p>
+
+    <.artist_card_album_info artist={@artist} />
     """
   end
 
@@ -192,7 +195,9 @@ defmodule TunezWeb.Artists.IndexLive do
     [
       {"recently updated", "-updated_at"},
       {"recently added", "-inserted_at"},
-      {"name", "name"}
+      {"name", "name"},
+      {"number of albums", "-album_count"},
+      {"latest album release", "--latest_album_year_released"}
     ]
   end
 
