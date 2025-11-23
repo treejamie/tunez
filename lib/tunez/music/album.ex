ccef4d3b36ff369a6e9ad9834ea114902178a8e3
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-11-23 08:53:35 +0000

    3-4:  Calculations for fields that don't exist (#7)
    now using some basic calculations to supply bits of aggregate data

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -4,14 +4,39 @@ defmodule Tunez.Music.Album do
   postgres do
     table "albums"
     repo Tunez.Repo
+
     references do
       reference :artist, index?: true, on_delete: :delete
     end
   end
 
-  identities do
-    identity :unique_album_names_per_artist, [:name, :artist_id],
-    message: "already exists for this artist"
+  actions do
+    defaults [:read, :destroy]
+
+    create :create do
+      accept [:name, :year_released, :cover_image_url, :artist_id]
+    end
+
+    update :update do
+      accept [:name, :year_released, :cover_image_url]
+    end
+  end
+
+  validations do
+    validate numericality(
+               :year_released,
+               greater_than_or_equal_to: 1950,
+               less_than_or_equal_to: &__MODULE__.next_year/0
+             ),
+             where: [present(:year_released)],
+             message: "must be between 1950 and next year"
+
+    validate match(
+               :cover_image_url,
+               ~S{^(https://|/images/).+}
+             ),
+             where: [changing(:cover_image_url)],
+             message: "must start with https:// or /images"
   end
 
   attributes do
@@ -37,32 +62,18 @@ defmodule Tunez.Music.Album do
     end
   end
 
-  actions do
-    defaults [:read, :destroy]
-    create :create do
-      accept [:name, :year_released, :cover_image_url, :artist_id]
-    end
-    update :update do
-      accept [:name, :year_released, :cover_image_url]
-    end
-  end
+  calculations do
+    calculate :years_ago, :integer, expr(2025 - year_released)
 
-  validations do
-    validate numericality(
-        :year_released,
-        greater_than_or_equal_to: 1950,
-        less_than_or_equal_to: &__MODULE__.next_year/0
-      ),
-      where: [present(:year_released)],
-      message: "must be between 1950 and next year"
-
-      validate match(:cover_image_url,
-         ~S{^(https://|/images/).+}
-      ),
-      where: [changing(:cover_image_url)],
-      message: "must start with https:// or /images"
+    calculate :string_years_ago,
+              :string,
+              expr("Wow, this was released " <> years_ago <> " years ago!")
   end
 
   def next_year, do: Date.utc_today().year + 1
 
+  identities do
+    identity :unique_album_names_per_artist, [:name, :artist_id],
+      message: "already exists for this artist"
+  end
 end
